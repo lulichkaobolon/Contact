3cc7895e8a6faa58f385ef0f4ffc982d86580bf1
lulichkaobolon <lulichkaobolon@gmail.com>
2020-07-26 18:41:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/contact/ContactInfoEdit.java
@@ -0,0 +1,55 @@
+package com.example.contact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ContactInfoEdit extends AppCompatActivity {
+    EditText editEmail, editDate, editGender, editCity;
+    TextView tv;
+    Integer index = MainActivity.selectedID;
+    Button btnOk, btnCancel;
+    DatabaseHelper myDb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info_edit);
+        myDb = new DatabaseHelper(this);
+        editEmail = findViewById(R.id.et_email);
+        editDate = findViewById(R.id.et_date);
+        editGender = findViewById(R.id.et_gender);
+        editCity = findViewById(R.id.et_city);
+        btnOk = findViewById(R.id.btn_ok);
+        btnCancel = findViewById(R.id.btn_cancel);
+        update();
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public void update () {
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdated = myDb.updateData(editEmail.getText().toString(),
+                        editDate.getText().toString(),
+                        editGender.getText().toString(),
+                        editCity.getText().toString());
+                if(isUpdated) {
+                    Toast.makeText(ContactInfoEdit.this, "Updated", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(ContactInfoEdit.this, "Not updated", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/contact/Line.java
@@ -0,0 +1,15 @@
+package com.example.contact;
+
+class Line {
+    public static final String keyName = "name", keyPhone = "phone", keyImage = "image";
+    String name, phone;
+    int id;
+    Line (String name, String phone, int id) {
+        class ExtraInfo {
+            String email, date, gender, city;
+        }
+        this.name = name;
+        this.phone = phone;
+        this.id = id;
+    }
+}
